1abd79dfe82fdf64c9806ad600f381469dad6ad1
Paul Statham <pstatham@sefas.com>
2014-08-18 15:32:13 +0100

    MODEL-3 Filter POJO added

---
+++ src/main/java/com/tvfreakz/model/ChannelProgrammeFilter.java
@@ -0,0 +1,52 @@
+/**
+ * TVFreakZ (c) 2014 - Paul Statham
+ */
+package com.tvfreakz.model;
+
+/**
+ * ChannelProgrammeFilter
+ * @author pstatham
+ *
+ */
+public class ChannelProgrammeFilter {
+  
+  private boolean deafSigned;
+  private boolean subtitled;
+  private boolean film;
+  private Genre genre;
+  
+  public Genre getGenre() {
+    return genre;
+  }
+  
+  public boolean isDeafSigned() {
+    return deafSigned;
+  }
+  
+  public boolean isFilm() {
+    return film;
+  }
+  
+  public boolean isSubtitled() {
+    return subtitled;
+  }
+  
+  public void setDeafSigned(boolean deafSigned) {
+    this.deafSigned = deafSigned;
+  }
+  
+  public void setFilm(boolean film) {
+    this.film = film;
+  }
+  
+  public void setGenre(Genre genre) {
+    this.genre = genre;
+  }
+  
+  public void setSubtitled(boolean subtitled) {
+    this.subtitled = subtitled;
+  }
+  
+  
+
+}
